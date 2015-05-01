d659ace4dc99813ca840dc1fd6a298cee58e74d4
Björn Ingi Baldvinsson <bjornb13@ru.is>
2015-05-01 19:15:49 +0000

    Crossing changes

--- crossing/crossing.c
+++ crossing/crossing.c
@@ -1,5 +1,30 @@
 #include "crossing.h"
 
+struct crossing state_c ;
+
+bool started_s = false;
+unsigned int K_s;
+sem_t light_s;
+sem_t turn_v[MAX_TYPE];
+sem_t turn_h[MAX_TYPE];
+
 void start_b(unsigned int k, size_t MAX_SPAWNS) {
 
 }
+
+void try_cross_ped(unsigned int dir){
+	sem_wait(&light_s);
+	if(dir == VERTICAL && c.crossing_e[VEHICLE] != 0){
+		waiting(PEDESTRIAN, dir);
+		sem_post(&light_s);
+		sem_wait(&turn_v[PEDESTRIAN]);
+	}
+	else if(dir == HORIZONTAL && c.crossing_n[VEHICLE] != 0){
+		waiting(PEDESTRIAN, dir);
+		sem_post(&light_s);
+		sem_wait(&turn_h[PEDESTRIAN]);
+	}
+	
+	
+}
+void try_cross_veh(unsigned int dir);
