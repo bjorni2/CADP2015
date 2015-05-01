eec31b6bc381f1e2ee128db4de433608d04fb0e2
Björn Ingi Baldvinsson <bjornb13@ru.is>
2015-05-01 19:04:42 +0000

    crossing prototypes

--- crossing/crossing.h
+++ crossing/crossing.h
@@ -19,5 +19,19 @@ struct crossing {
 	unsigned int waiting_e[2];
 } state_c;
 
+extern bool started_s;
+extern unsigned int K_s;
+extern sem_t light_s;
+extern sem_t turn[MAX_TYPE];
+
+void try_cross_ped(unsigned int dir);
+void try_cross_veh(unsigned int dir);
+
 void start_c(unsigned int k, size_t MAX_SPAWNS);
 
+void signal_c(unsigned int type);
+
+inline void inc_cross_c(unsigned int type);
+inline void done_crossing_c(unsigned int type);
+inline void waiting(unsigned int type);
+inline void not_waiting(unsigned int type);
