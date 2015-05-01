cc065728540e59be6447a7f16c5e6a53025808b9
throsturx <sparog+github@gmail.com>
2015-05-01 14:31:39 +0000

    fixed compile errors

--- crossing/main.c
+++ crossing/main.c
@@ -18,14 +18,14 @@ int main(int argc, char * argv[]) {
 	}
 	pthread_t pt_ui;
 	FILE *fp = fopen("log.txt", "w");
-	init_log(fp);
+	init_log(stdout);
 	
 	// TODO: switch chosen_crossing
 	// spawn correct controller
 
-	pthread_create(&pt_ui, NULL, draw_s, (void *) &state_s);
-	start_s(3, 900);
-	pthread_join(pt_ui, NULL);
+//	pthread_create(&pt_ui, NULL, draw_s, (void *) &state_s);
+	start_s(3, 100);
+//	pthread_join(pt_ui, NULL);
 
 	return 0;
 }
--- crossing/simple.c
+++ crossing/simple.c
@@ -31,7 +31,7 @@ void start_s(unsigned int k, size_t MAX_SPAWNS) {
 	while (true) {
 		bool stop = true;
 		milli_sleep(2);	
-		log_sem(-1);
+//		log_sem(-1);
 		for (i = 0; i < MAX_TYPE; ++i) {
 			if (atomic_load(&(state_s.waiting[i]))) stop = false;
 			if (atomic_load(&(state_s.crossing[i]))) stop = false;
@@ -59,10 +59,10 @@ void try_cross_s(unsigned int type) {
 		sem_post(&light_s);
 		sem_wait(&turn[type]);
 	}
-	sate_s.k[!type]--;
+	state_s.k[!type]--;
 	inc_cross_s(type);
 	signal_s();
-	rand_sleep(10);
+	rand_sleep(1000);
 	sem_wait(&light_s);
 	done_crossing_s(type);
 	signal_s();
@@ -123,11 +123,11 @@ inline bool can_cross_s(unsigned int type) {
 	val = (state_s.turn == type);
 
 	if (val && state_s.timeout) {
-		unsigned int k = state_s.k;
+		unsigned int k = state_s.k[type];
 		if (k == 0) {
 			return false;
 		}
-		state_s.k = k - 1;
+		state_s.k[type] = k - 1;
 		return true;
 	}
 
