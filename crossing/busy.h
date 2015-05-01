7d299d575805bca384c64d1dc83b7c567040a8cd
throsturx <sparog+github@gmail.com>
2015-05-01 18:54:23 +0000

    small fixes

--- crossing/busy.h
+++ crossing/busy.h
@@ -3,3 +3,5 @@
 struct busy {
 	int a;
 } state_b;
+
+void start_b(unsigned int k, size_t MAX_SPAWNS);
--- crossing/crossing.h
+++ crossing/crossing.h
@@ -4,3 +4,5 @@ struct crossing {
 	int a;
 } state_c;
 
+void start_c(unsigned int k, size_t MAX_SPAWNS);
+
