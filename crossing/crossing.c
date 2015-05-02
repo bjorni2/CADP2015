0067bfb184109509413a2e4d3c8448e2fe44608f
Björn Ingi Baldvinsson <bjornb13@ru.is>
2015-05-02 12:59:10 +0000

    vehicle k

--- crossing/crossing.c
+++ crossing/crossing.c
@@ -137,24 +137,71 @@ void try_cross_ped(unsigned int dir){
 
 void try_cross_veh(unsigned int dir){
 	sem_wait(&light_c);
-	if(dir == VERTICAL && (state_c.crossing_h[VEHICLE] != 0 || state_c.crossing_h[PEDESTRIAN] != 0)){
+	if((dir == VERTICAL && (state_c.crossing_h[VEHICLE] != 0 || state_c.crossing_h[PEDESTRIAN] != 0)) || state_c.k[HORIZONTAL] == 0){
 		waiting_c(VEHICLE, dir);
+		if(state_c.waiting_v[PEDESTRIAN] == 0 && state_c.waiting_v[VEHICLE] == 1){
+			state_c.k[VERTICAL] = K_c;
+		}
 		sem_post(&light_c);
 		sem_wait(&turn_v[VEHICLE]);
 	}
-	else if(dir == HORIZONTAL && (state_c.crossing_v[VEHICLE] != 0 || state_c.crossing_v[PEDESTRIAN] != 0)){
+	else if((dir == HORIZONTAL && (state_c.crossing_v[VEHICLE] != 0 || state_c.crossing_v[PEDESTRIAN] != 0)) || state_c.k[VERTICAL] == 0){
 		waiting_c(VEHICLE, dir);
+		if(state_c.waiting_h[PEDESTRIAN] == 0 && state_c.waiting_h[VEHICLE] == 1){
+			state_c.k[HORIZONTAL] = K_c;
+		}
 		sem_post(&light_c);
 		sem_wait(&turn_h[VEHICLE]);
 	}
 	
+	if(dir == VERTICAL && (state_c.waiting_h[VEHICLE] > 0 || state_c.waiting_h[PEDESTRIAN])){
+		state_c.k[HORIZONTAL]--;
+	}
+	else if(dir == HORIZONTAL && (state_c.waiting_v[VEHICLE] > 0 || state_c.waiting_v[PEDESTRIAN] > 0)){
+		state_c.k[VERTICAL]--;
+	}
+	
+	
 	inc_cross_c(VEHICLE, dir);
-	signal_c();
+	
+	
+	if(dir == VERTICAL && (state_c.waiting_h[VEHICLE] > 0 || state_c.waiting_h[PEDESTRIAN]) && state_c.k[HORIZONTAL] == 0){ // Ef k er 0 megum við bara opna light_s
+		sem_post(&light_s);
+	}
+	else if(dir == HORIZONTAL && (state_c.waiting_v[VEHICLE] > 0 || state_c.waiting_v[PEDESTRIAN] > 0) && state_c.k[VERTICAL] == 0){
+		sem_post(&light_s);
+	}
+	else{
+		signal_c();
+	}
 	//crossing, sleep?
 	rand_sleep(500);
 	sem_wait(&light_c);
 	done_inc_cross_c(VEHICLE, dir);
-	signal_c();
+	
+	if(dir == VERTICAL && state_c.crossing_v[PEDESTRIAN] == 0 && state_c.crossing_v[VEHICLE] == 0 && state_c.waiting_h[VEHICLE] > 0){
+		state_c.k[HORIZONTAL] = K_c;
+		not_waiting_c(VEHICLE, HORIZONTAL);
+		sem_post(&turn_h[VEHICLE]);
+	}
+	else if(dir == VERTICAL && state_c.crossing_v[VEHICLE] == 0 && state_c.waiting_h[PEDESTRIAN] > 0 && state_c.waiting_h[VEHICLE] == 0){
+		state_c.k[HORIZONTAL] = K_c;
+		not_waiting_c(PEDESTRIAN, HORIZONTAL);
+		sem_post(&turn_h[PEDESTRIAN]);
+	}
+	else if(dir == HORIZONTAL && state_c.crossing_h[PEDESTRIAN] == 0 && state_c.crossing_h[VEHICLE] == 0 && state_c.waiting_v[VEHICLE] > 0){
+		state_c.k[VERTICAL] = K_c;
+		not_waiting_c(VEHICLE, VERTICAL);
+		sem_post(&turn_v[VEHICLE]);
+	}
+	else if(dir == HORIZONTAL && state_c.crossing_h[VEHICLE] == 0 && state_c.waiting_v[PEDESTRIAN] > 0 && state_c.waiting_v[VEHICLE] == 0){
+		state_c.k[VERTICAL] = K_c;
+		not_waiting_c(PEDESTRIAN, VERTICAL);
+		sem_post(&turn_h[VERTICAL]);
+	}
+	else{
+		signal_c();
+	}
 }
 
 void signal_c(){
